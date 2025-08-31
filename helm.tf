831db5d5fd025f529f7387728f25c7cfa05476f1
Donald Etsecom <detsecom@ashleyfurniture.com>
2025-08-31 18:46:44 -0500

    dev

--- helm.tf
+++ helm.tf
@@ -1,7 +1,8 @@
-module "helm_config" {
-  source                = "./modules/helm"
-  vault_operator_values = local.vault_values
-  cloudflared_values    = local.cloudflared_values
-  cloudflared_namespace = var.cloudflare_namespace["cloudflared"]
-  depends_on            = [null_resource.kustomization]
-}
+# module "helm_config" {
+#   source                = "./modules/helm"
+#   vault_operator_values = local.vault_values
+#   cloudflared_values    = local.cloudflared_values
+#   cloudflared_namespace = var.cloudflare_namespace["cloudflared"]
+#   depends_on            = [null_resource.kustomization]
+# }
+# # 
