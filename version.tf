2df9f101f53211212c0d3eae0467fdd5538c5c53
Donald Etsecom <detsecom@ashleyfurniture.com>
2025-08-31 14:16:34 -0500

    version

---
+++ version.tf
@@ -0,0 +1,18 @@
+terraform {
+  # required_version = ">= 1.1.0"
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.10"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.19"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.21"
+    }
+  }
+
+}
