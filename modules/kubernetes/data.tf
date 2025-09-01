1d15f10139b3ebd337327162263fb1dc22865036
Donald Etsecom <detsecom@ashleyfurniture.com>
2025-08-31 20:11:57 -0500

    vault

---
+++ modules/kubernetes/data.tf
@@ -0,0 +1,10 @@
+data "kubernetes_secret_v1" "vault_seal" {
+  # count = var.enable_vault ? 1 : 0
+
+  metadata {
+    name      = "vault-unseal-keys"
+    namespace = "vault"
+  }
+
+  depends_on = [time_sleep.sleep-wait-vault]
+}
