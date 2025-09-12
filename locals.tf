ba72ede9338a535e303cae52376241fd0629d191
Donald Etsecom <detsecom@ashleyfurniture.com>
2025-09-12 02:48:15 -0500

    nfs csi

--- locals.tf
+++ locals.tf
@@ -594,7 +594,7 @@ clusterResourceNamespace: cert-manager
       parameters:
         server: 10.1.10.11
         share: /var/nfs/shared/rke2_prod_data
-        subDir: /$${pvc.namespace}/${pvc.name}
+        # subDir: ""
         mountPermissions: "0"
     #     csi.storage.k8s.io/provisioner-secret is only needed for providing mountOptions in DeleteVolume
         # csi.storage.k8s.io/provisioner-secret-name: "mount-options"
