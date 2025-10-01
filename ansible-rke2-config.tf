1d16a7cc7b17b9b778497cf779971747e96efd7c
Donald Etsecom <donydonald1@icloud.com>
2025-10-01 05:14:10 -0500

    rebrand

--- ansible-rke2-config.tf
+++ ansible-rke2-config.tf
@@ -12,24 +12,24 @@ rke2_airgap_mode: false
 rke2_ha_mode: true
 rke2_ha_mode_keepalived: false
 rke2_api_ip: ${var.manager_rke2_api_ip}
-rke2_token: "${random_password.rke2_token.result}"
+rke2_token: ${random_password.rke2_token.result}
 rke2_debug: true
 rke2_wait_for_all_pods_to_be_ready: ${var.wait_for_all_pods_to_be_ready}
 rke2_additional_sans: [
 %{for item in local.all_sans~}
-  "${item}"%{if index(local.all_sans, item) < length(local.all_sans) - 1}, %{endif}
+  ${item}%{if index(local.all_sans, item) < length(local.all_sans) - 1}, %{endif}
 %{endfor~}
 ]
 rke2_download_kubeconf: true
 rke2_download_kubeconf_file_name: "${var.cluster_name}-kubeconfig.yaml"
 rke2_download_kubeconf_path: "${path.cwd}/"
-rke2_cluster_group_name: "${var.cluster_name}"
+rke2_cluster_group_name: ${var.cluster_name}
 rke2_channel: stable
 rke2_cis_profile: "cis"
 rke2_kubevip_image: ghcr.io/kube-vip/kube-vip:${var.kubevip_version}
 
 rke2_loadbalancer_ip_range: 
-  range-global: "${var.manager_rke2_loadbalancer_ip_range}"
+  range-global: ${var.manager_rke2_loadbalancer_ip_range}
   allow-share-range-global: "true"
   # allow-share-ingress-nginx: "true"
   allow-share-logging: "true"
@@ -43,7 +43,7 @@ rke2_kubevip_cloud_provider_enable: true
 rke2_kubevip_cloud_provider_image: ghcr.io/kube-vip/kube-vip-cloud-provider:${var.kubevip_cloud_provider_image_tag}
 rke2_kubevip_svc_enable: true
 # rke2_kubevip_metrics_port: 2112
-rke2_version: "${var.rke2_version}"
+rke2_version: ${var.rke2_version}
 # rke2_apiserver_dest_port: 6443
 # rke2_interface: enp0s31f6
 rke2_kubevip_args:
@@ -115,18 +115,18 @@ rke2_service_cidr:
   - 10.43.0.0/16
 
 rke2_etcd_snapshot_s3_options:
-  etcd-s3: "${var.enable_rke2_etcd_s3_backup}"
-  s3_endpoint: "${var.s3_backup_endpoint}" 
-  access_key: "${var.s3_backup_access_key}" 
-  secret_key: "${var.s3_backup_secret_key}" 
-  bucket: "${var.s3_backup_bucketname}" 
-  snapshot_name: "${var.snapshot_name}" 
-  etcd-snapshot-retention: "${var.etcd_snapshot_retention}"
-  etcd-snapshot-schedule-cron: "${var.etcd_snapshot_schedule_cron}"
+  etcd-s3: ${var.enable_rke2_etcd_s3_backup}
+  s3_endpoint: ${var.s3_backup_endpoint}
+  access_key: ${var.s3_backup_access_key}
+  secret_key: ${var.s3_backup_secret_key}
+  bucket: ${var.s3_backup_bucketname}
+  snapshot_name: ${var.snapshot_name}
+  etcd-snapshot-retention: ${var.etcd_snapshot_retention}
+  etcd-snapshot-schedule-cron: ${var.etcd_snapshot_schedule_cron}
   skip_ssl_verify: ${var.s3_backup_skip_ssl_verify} 
   endpoint_ca: "" 
   region: "" 
-  folder: "${var.cluster_name}" 
+  folder: ${var.cluster_name}
 
 # rke2_ingress_controller: ingress-nginx
 # rke2_ingress_nginx_values:
