69930cec552337945100e9a54225982188933c79
Donald Etsecom <donydonald1@icloud.com>
2025-10-26 21:13:34 -0500

    rke2 rebrand

--- kubeconfig.tf
+++ kubeconfig.tf
@@ -2,7 +2,7 @@ module "kubeconfig" {
   source                    = "./modules/helm-k8s-config/"
   create_kubeconfig         = var.create_kubeconfig
   cluster_name              = var.cluster_name
-  kubeconfig_server_address = var.control_plane_hosts[0].ip
+  kubeconfig_server_address = var.control_plane_servers[0].ip
   enable_rke2_cluster_api   = var.enable_rke2_cluster_api
   manager_rke2_api_dns      = var.manager_rke2_api_dns
   ssh_private_key           = file(var.ssh_private_key_file)
